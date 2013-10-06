c7b0d098250afd253744eaface9cba9d5cef64b4
William Mayor <mail@williammayor.co.uk>
2013-10-06 18:36:08 +0100

    Homepage getting there

---
+++ js/main.js
@@ -0,0 +1,38 @@
+HOME = {
+    init: function() {
+        HOME.countdown();
+        window.setInterval(HOME.countdown, 1000);
+    }, 
+    countdown: function() {
+        var tick = Date.now();
+        var tock = new Date(2015, 7, 1, 14, 0, 0);
+        var delta = tock - tick; // milliseconds
+        var days = Math.floor(delta / (1000*60*60*24));
+        var hours = Math.floor(delta / (1000*60*60)) - days*24;
+        var minutes = Math.floor(delta / (1000*60)) - days*24*60 - hours*60;
+        $(".countdown .days span.number").text(days);
+        $(".countdown .hours span.number").text(hours);
+        $(".countdown .minutes span.number").text(minutes);
+        $(".countdown .days span.plural").text((days === 1) ? '' : 's');
+        $(".countdown .hours span.plural").text((hours === 1) ? '' : 's');
+        $(".countdown .minutes span.plural").text((minutes === 1) ? '' : 's');
+    }
+};
+COMMON = {
+    init: function() {
+    }
+};
+APP = {
+    common: COMMON.init,
+    home: HOME.init
+};
+UTIL = {
+    fire: function(id) {
+        APP[id]();
+    },
+    loadEvents : function() {
+        UTIL.fire('common', []);
+        UTIL.fire(document.body.id);
+    }
+};
+$(document).ready(UTIL.loadEvents);
